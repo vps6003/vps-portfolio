00d66811792f3ae1f1aeb76cc4e45623f7b544f2
vps6003 <vaibhavsaraf.2007@gmail.com>
2025-10-20 15:49:45 +0530

    Html Deployent render fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-    port : 4200, // changing local port
-  },
-  base : '/',
-})
+  plugins: [react()],
+  base: '/', // MUST be '/' for Vercel SPA deployment
+});
